6bfb37ba29abf818927c9a15d065a03d7f1a20fb
AustinKurtis <austin.jay.kurtis@gmail.com>
2018-02-06 11:09:53 -0600

    finished lesson 1 working on lesson 2

--- Lesson3-JS-II/homework/homework.js
+++ Lesson3-JS-II/homework/homework.js
@@ -3,6 +3,13 @@
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+  if (x > y) {
+    return x;
+  } else if (x < y) {
+    return y;
+  } else if ( x == y) {
+    return x;
+  }
 }
 
 function greeting(language) {
@@ -11,15 +18,30 @@ function greeting(language) {
   // language: 'English' -> 'Hello!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
+  if (language = 'German') {
+    return 'Guten Tag!';
+  }else if (language = 'English') {
+    return 'Hello!';
+  }else if (language = 'Spanish') {
+    return 'Hola!';
+  }else {
+    return 'Hello!';
+  }
 }
 
 function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
+  if (num == 10 || num == 5) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
+  return num < 50 || num > 20;
 }
 
 function isInteger(num) {
@@ -29,6 +51,9 @@ function isInteger(num) {
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
+  if (Math.floor(num) <= 1){
+    return true;
+  }
 }
 
 function fizzBuzz(num) {
@@ -36,6 +61,17 @@ function fizzBuzz(num) {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  for (i = 0; i < num; i ++){
+  	if ( i % 3 == 0 && i % 5 == 0){
+    	return('fizzbuzz');
+    }else if (i % 3 == 0){
+      return('fizz');
+    }else if (i % 5 == 0){
+      return('buzz');
+    }else{
+      return(i);
+    }
+  }
 }
 
 function isPrime(num) {
@@ -44,24 +80,35 @@ function isPrime(num) {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+    if (num == 0 || num == 1) {
+      return false;
+    } else if (num / num == 1) {
+      return true;
+    } else {
+      return false;
+    }
 }
 
 function returnFirst(arr) {
   // return the first item from the array
+  return arr[0];
 }
 
 function returnLast(arr) {
   // return the last item of the array
+  return arr[arr.length];
 }
 
 function getArrayLength(arr) {
   // return the length of the array
+  return arr.length;
 }
 
 function incrementByOne(arr) {
   // arr is an array of integers  
   // increase each integer by one
   // return the array
+  
 }
 
 function addItemToArray(arr, item) {
